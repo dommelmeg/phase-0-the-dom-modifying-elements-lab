2fa5efa85991cc15a5f265b3956ebe89724d5755
Megan <dommelmeg@gmail.com>
2022-11-03 15:19:58 -0400

    commit tree aa864192d24a10d031cb98aaeb2c47dc1de17af2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.querySelector('#main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+document.body.append(newHeader)
+
+document.body.querySelector('h1').id = 'victory'
+
+document.body.querySelector('h1').innerHTML = 'YOUR-NAME is the champion'
